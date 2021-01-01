269f369f5e3604c7cf9a67a0e6871931243d0008
Iuri Pereira Viana <iuripereirav@gmail.com>
2020-12-31 23:33:09 -0300

    Projeto finalizado

--- OtakuWorld/scripts/Produtos.js
+++ OtakuWorld/scripts/Produtos.js
@@ -1,7 +1,6 @@
+/*Ativar menu lateral*/
 var MenuLateral = document.querySelector(".MenuLateral");
-
 function Ativar(){
-
     if(MenuLateral.style.left === ""){
         MenuLateral.style.left = "0vw";
     }
@@ -10,56 +9,79 @@ function Ativar(){
     }
 }
 
+/*Slide*/ 
+
 var intervalo = 3000;
 var banner = document.querySelector("#banner")
+var keyframe = [{opacity:'0.4'},{opacity:'1'}]
 
 function slide1(){
     banner.src = "./images/Slides/Slide1.jpg"
+    banner.animate(keyframe, {duration: 2000, iterations: 1});
     setTimeout("slide2()", intervalo);
-    banner.style.opacity = "1"
 }
 
 function slide2(){
+    banner.animate(keyframe, {duration: 2000, iterations: 1});
     banner.src = "./images/Slides/Slide2.jpg"
     setTimeout("slide3()", intervalo);
-    banner.style.opacity = "0.9"
 }
 
 function slide3(){
+    banner.animate(keyframe, {duration: 2000, iterations: 1});
     banner.src = "./images/Slides/Slide3.jpg"
     setTimeout("slide4()", intervalo);
-    banner.style.opacity = "1"
 }
 
 function slide4(){
+    banner.animate(keyframe, {duration: 2000, iterations: 1});
     banner.src = "./images/Slides/Slide4.jpg"
     setTimeout("slide1()", intervalo);
-    banner.style.opacity = "0.9"
 }
 
+/**Tema e localStorage*/
 
-var header = document.querySelector('header.principal');
+var header2 = document.querySelector('header.principal');
 var SlideAndTitle = document.querySelector('.SlideAndTitle');
 var title = document.querySelector('.title');
 var produtos = document.querySelector('#produtos');
 var End = document.querySelector('#End');
+var IconLamp2 = document.querySelector('img.IconLamp')
 
+header2.style.backgroundColor = localStorage.header2
+SlideAndTitle.style.backgroundColor = localStorage.SlideAndTitle
+title.style.backgroundColor = localStorage.title
+produtos.style.backgroundColor= localStorage.produtos;
+End.style.backgroundColor = localStorage.End;
+IconLamp2.src = localStorage.IconLamp2;
 
-function Tema(){
-    if(title.style.backgroundColor== "rgb(80, 80, 80)"){
-        header.style.backgroundColor = "";
-        title.style.backgroundColor= "";
-        SlideAndTitle.style.backgroundColor= "";
-        produtos.style.backgroundColor= "";
-        End.style.backgroundColor= "";
-        }
-    
-        else{
-        header.style.backgroundColor= "rgb(100, 100, 100, 0.9)";
-        title.style.backgroundColor= "rgb(80, 80, 80)";
-        SlideAndTitle.style.backgroundColor= "rgb(130, 130, 130)";
-        produtos.style.backgroundColor= "rgb(120, 120, 120)";
-        End.style.backgroundColor= "rgb(130, 130, 130)";
+    var SalvarDados = function() {
+        localStorage.setItem('header2', header2.style.backgroundColor);
+        localStorage.setItem('SlideAndTitle', SlideAndTitle.style.backgroundColor);
+        localStorage.setItem('title', title.style.backgroundColor);
+        localStorage.setItem('produtos', produtos.style.backgroundColor);
+        localStorage.setItem('End', End.style.backgroundColor);
+        localStorage.setItem('IconLamp2', IconLamp2.src);
+    }
+    document.onchange = SalvarDados;
 
-        }
-}
+    function Tema(){
+        if(title.style.backgroundColor== "rgb(80, 80, 80)"){
+            header2.style.backgroundColor = "";
+            title.style.backgroundColor= "";
+            SlideAndTitle.style.backgroundColor= "";
+            IconLamp2.src = "./images/Svg/Lua.svg"
+            produtos.style.backgroundColor= "";
+            End.style.backgroundColor= "";
+            }
+        
+            else{
+            header2.style.backgroundColor= "rgb(100, 100, 100, 0.9)";
+            title.style.backgroundColor= "rgb(80, 80, 80)";
+            SlideAndTitle.style.backgroundColor= "rgb(130, 130, 130)";
+            IconLamp2.src = "./images/Svg/Sol.svg"
+            produtos.style.backgroundColor= "rgb(120, 120, 120)";
+            End.style.backgroundColor= "rgb(130, 130, 130)";
+            }
+    }
+
--- OtakuWorld/scripts/Test.js
+++ OtakuWorld/scripts/Test.js
@@ -1,34 +1,31 @@
-var back = document.body;
-
-var header = document.querySelector('header.principal');
+/*Tema, mas sem localStorage*/
+var back = document.body
+var header = document.querySelector('header.principal')
 var Hs = document.querySelector('header.second')
-var Lamp = document.querySelector('.containerLamp');
+var Lamp = document.querySelector('.containerLamp')
 var IconLamp = document.querySelector('img.IconLamp')
 var Aside = document.querySelector('#AsideImg');
-var bar = document.querySelector('.barrinha');
-
+var bar = document.querySelector('.barrinha')
 
 function Tema(){
-
+    
     if(back.style.backgroundColor == "rgb(135, 140, 145)"){
-        
-    back.style.backgroundColor = "";
-    header.style.backgroundColor = "";
-    Lamp.style.backgroundColor = "";
-    IconLamp.src = "./images/Lua.svg"
-    bar.style.backgroundColor = "";
-    Aside.style.filter = "";
-    Hs.style.backgroundColor = "";
-    }
-
-    else{
-
-    back.style.backgroundColor="rgb(135, 140, 145)";
-    header.style.backgroundColor= "rgb(85, 85, 85)";
-    Lamp.style.backgroundColor= "rgb(105, 110, 115)";
-    IconLamp.src = "./images/Sol.svg"
-    Aside.style.filter = "grayscale(55%)";
-    bar.style.backgroundColor= "rgb(85, 85, 85)";
-    Hs.style.backgroundColor = "rgb(105, 110, 115)";
-    }
+        back.style.backgroundColor = "";
+        header.style.backgroundColor = "";
+        Lamp.style.backgroundColor = "";
+        IconLamp.src = "./images/Svg/Lua.svg"
+        bar.style.backgroundColor = "";
+        Aside.style.filter = "";
+        Hs.style.backgroundColor = "";
+        }
+        else{
+        back.style.backgroundColor="rgb(135, 140, 145)";
+        header.style.backgroundColor= "rgb(85, 85, 85)";
+        Lamp.style.backgroundColor= "rgb(105, 110, 115)";
+        IconLamp.src = "./images/Svg/Sol.svg"
+        Aside.style.filter = "grayscale(55%)";
+        bar.style.backgroundColor= "rgb(85, 85, 85)";
+        Hs.style.backgroundColor = "rgb(105, 110, 115)";
+        }
 }
+
